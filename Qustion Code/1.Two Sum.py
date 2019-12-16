95186c7842066a9e5ddf090e78de61eb70967868
Xin Zhengfang <xinzhengfang@qq.com>
2019-12-16 20:41:57 +0800

    update  Q1

--- Qustion Code/1.Two Sum.py
+++ Qustion Code/1.Two Sum.py
@@ -7,15 +7,17 @@ class Solution(object):
         """
 
         """
-        # Try one by one （n-1)!
+        # Try one by one
+        # time complexity: n^2
+        # space complexity: 1
         for i in range(len(nums)):
-            for j in range(len(nums)-i-1):
+            for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
         """
 
         # The implementation as below is bit tricky. but not difficult to understand
-        #  1) Traverse the array one by one
+        #  1) loop though the array 
         #  2) just put the `target - num[i]`(not `num[i]`) into the map
         # @haoel's thoughts[https://github.com/haofengsiji/leetcode/blob/master/algorithms/cpp/twoSum/twoSum.cpp]
         # Code reference from @qiyuangong's code [https://github.com/qiyuangong/leetcode/blob/master/python/001_Two_Sum.py]
