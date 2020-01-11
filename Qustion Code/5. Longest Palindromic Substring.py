5d127a8100f1f3bab24aa338e9df2740afcad098
Xin Zhengfang <xinzhengfang@qq.com>
2020-01-11 11:59:56 +0800

    Q5 A3

--- Qustion Code/5. Longest Palindromic Substring.py
+++ Qustion Code/5. Longest Palindromic Substring.py
@@ -45,8 +45,45 @@ class Solution:
         
 #         return s[ri:rj+1]
 
-# methode_3
-    
+# # methode_3
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+#         size = len(s)
+#         ri = 0
+#         rj = 0
+#         max_count = 0 
+#         if size == 0:
+#             return s
+
+#         # even substr
+#         for i in range(size-1):
+#             for j in range(min(i+1,size-(i+1))):
+#                 if s[i-j] == s[i+1+j]:
+#                     if 2*(j+1) > max_count:
+#                         ri = i
+#                         rj = j 
+#                         max_count = 2*(j+1)
+#                         flag = True       
+#                 else:
+#                     break
+        
+        
+#         # odd substr
+#         for i in range(size):
+#             for j in range(min(i+1,size-(i+1)+1)):
+#                 if s[i-j] == s[i+j]:
+#                     if 2*j+1 > max_count:
+#                         ri = i
+#                         rj = j 
+#                         max_count = 2*j+1
+#                         flag = False
+#                 else:
+#                     break
+        
+#         if flag == True:
+#             return s[ri-rj:(ri+1+rj)+1]
+#         else:
+#             return s[ri-rj:(ri+rj)+1]   
 
 if __name__ == "__main__":
 
