34b4f9f4ad8261ab451b2591612bb674c2d7177a
Xin Zhengfang <xinzhengfang@qq.com>
2020-01-13 09:57:55 +0800

    Q6 A2

--- Qustion Code/6. ZigZag Conversion.py
+++ Qustion Code/6. ZigZag Conversion.py
@@ -12,8 +12,19 @@ class Solution:
             else:
                 i = (numRows-1)*2 - i
                 s_array[i] += c
-    
-        for substr in s_array:
-            zigzag += substr
         
-        return zigzag
+        return "".join(s_array)
+
+# # method_2
+# class Solution:
+#     def convert(self, s: str, numRows: int) -> str:
+#         if numRows < 2:
+#             return s
+#         i,flag = 0,-1
+#         s_array = ["" for _ in range(numRows)]
+#         for c in s:
+#             s_array[i] += c
+#             if i == 0 or i == numRows - 1: flag = -flag
+#             i += flag
+        
+#         return "".join(s_array)
