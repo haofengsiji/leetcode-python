25dcad7004ec99b61c6a116ce30a71fbcba2dcbd
Xin Zhengfang <xinzhengfang@qq.com>
2020-01-14 11:35:25 +0800

    Q7 A2

--- Qustion Code/7. Reverse Integer.py
+++ Qustion Code/7. Reverse Integer.py
@@ -9,3 +9,18 @@ class Solution:
             return sign*x
         else:
             return 0
+
+# # method_2
+# class Solution:
+#     def reverse(self, x: int) -> int:
+#         sign = 1 if x>0 else -1
+#         rev = 0
+#         x = abs(x)
+#         while(x != 0):
+#             pop = x % 10
+#             x = x//10
+#             if rev > 2**31//10: return 0
+#             if rev < -2**31//10: return 0
+#             rev = rev*10 + pop 
+#         return sign*rev 
+        
