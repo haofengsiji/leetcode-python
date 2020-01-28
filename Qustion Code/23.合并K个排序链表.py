4bb11d94edc8c6038dcf7744366ebd6aae31f4ce
haofengsiji <xinzhengfang@qq.com>
2020-01-28 17:59:25 +0800

    q23 a2a3

--- Qustion Code/23.合并K个排序链表.py
+++ Qustion Code/23.合并K个排序链表.py
@@ -31,3 +31,56 @@ class Solution:
         if right == None: p.next = left
 
         return dummy.next  
+
+# method_2:暴力法 还挺快的
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        nums = []
+        for l in lists:
+            while l != None:
+                nums.append(l.val)
+                l = l.next
+
+        points = ListNode(0)
+        p = points
+        for num in sorted(nums):
+            cur = ListNode(num)
+            p.next = cur
+            p = cur
+        return points.next
+
+# method_3: 优先队列，有点慢
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+from  queue import PriorityQueue
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+
+        q = PriorityQueue()
+        # 向优先队列里放置头节点的值和节点
+        for i,l in enumerate(lists):
+            if l:
+                # 多加一个index是为了防止比较节点，节点无法比较
+                q.put((l.val,i,l))
+        prehead = ListNode(0)
+        p = prehead
+        while not q.empty():
+            # 取出头节点最小的值，加入输出链表
+            val, index, node = q.get()
+            p.next = ListNode(val)
+            p = p.next
+            node = node.next
+            if node:
+                q.put((node.val,index,node))
+
+        return prehead.next
