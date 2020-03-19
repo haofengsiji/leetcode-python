bfca8bb532dcc3bbf4b3fc7810b07f70be2d5d5d
haofengsiji <xinzhengfang@qq.com>
2020-03-19 11:07:30 +0800

    q409 a1

--- Qustion Code/409. 最长回文串.py
+++ Qustion Code/409. 最长回文串.py
@@ -1,18 +1,10 @@
-import collections
+# method_1
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         ans = 0
         cnt = collections.Counter(s)
         for key in cnt:
-            ans += cnt[key]//2
+            ans += cnt[key]//2 * 2
         if ans < len(s):
             ans = ans+1
         return ans 
-
-
- 
-
-if __name__ == "__main__":
-    s = Solution()
-    print(s.longestPalindrome("abccccdd"))
-
