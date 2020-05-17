4ca621f4dc5b0250e5e64def1ff05d2ad9150643
haofengsiji <xinzhengfang@qq.com>
2020-05-17 23:12:57 +0800

    q260 a1a2

--- Qustion Code/210. 课程表 II.py
+++ Qustion Code/210. 课程表 II.py
@@ -1,6 +1,7 @@
-import collections
+# method_1
+#   DFS
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites):
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         # 存储有向图
         edges = collections.defaultdict(list)
         # 标记每个节点的状态: 0=未搜索 1=搜索中 2=搜索完成
@@ -46,10 +47,41 @@ class Solution:
         # 注意下标 0 为栈底，因此需要将数组反序输出
         return result[::-1]
 
+# Method_2 
+#   BFS
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # 存储有向图
+        edges = collections.defaultdict(list)
+        # 存储每个节点的入度
+        indeg = [0]*numCourses
+        # 列表模拟栈，下标0为栈底，n-1为栈顶
+        result = list()
+        # 判断图中是否有环
+        invalid  = False
 
- 
+        # 构建图
+        for info in prerequisites:
+            edges[info[1]].append(info[0])
+            indeg[info[0]] += 1
+        
+        # 将所有入度为零的节点放到队列中
+        q = collections.deque([u for u in range(numCourses) if indeg[u]==0])
 
-if __name__ == "__main__":
-    s = Solution()
-    print(s.findOrder(2,[[0,1]]))
+        while q:
+            # 从队首取出一个节点
+            u = q.popleft()
+            # 放入到答案中
+            result.append(u)
+            # 更新入度
+            for v in edges[u]:
+                indeg[v] -= 1
+                # 如果相邻的节点入度为0，则加入到队列当中
+                if indeg[v] == 0:
+                    q.append(v)
+        
+        # 没有答案
+        if len(result) != numCourses:
+            result = []
 
+        return result
