dce50d732bdc5d34110919081054ae9514c8fb7f
haofengsiji <xinzhengfang@qq.com>
2020-07-12 22:41:40 +0800

    update q175 a1a1.1a2a2.2

--- Qustion Code/174. 地下城游戏.py
+++ Qustion Code/174. 地下城游戏.py
@@ -1,3 +1,4 @@
+# method_1
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         rows = len(dungeon)
@@ -18,3 +19,73 @@ class Solution:
                 dp[i][j] = max(1,min(dp[i+1][j]-dungeon[i][j],dp[i][j+1]-dungeon[i][j]))
         
         return dp[0][0]
+
+# method_1.1
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        m = len(dungeon)
+        n = len(dungeon[0])
+        BIG = 10**9
+        dp = [[BIG]*(n+1) for _ in range(m+1)]
+        dp[m][n-1] = dp[m-1][n] = 1
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                minn = min(dp[i+1][j],dp[i][j+1])
+                dp[i][j] = max(minn-dungeon[i][j],1)
+        
+        return dp[0][0]
+
+# method_2 未优化
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        m = len(dungeon)
+        n = len(dungeon[0])
+        
+        def dfs(i,j):
+            # 到达最后一行，最后一列
+            if i == m-1 and j == n-1:
+                return max(1 - dungeon[i][j],1)
+            # 到达最后一行,向右搜索
+            if i == m-1:
+                return max(dfs(i,j+1) - dungeon[i][j],1)
+            # 到达最后一列，向下搜索
+            if j == n-1:
+                return max(dfs(i+1,j) - dungeon[i][j],1)
+            
+            # 向右+向下搜索
+            return max(min(dfs(i,j+1) - dungeon[i][j],dfs(i+1,j) - dungeon[i][j]), 1)
+        
+        return dfs(0,0)
+
+# method_2.1 memo 优化
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        m = len(dungeon)
+        n = len(dungeon[0])
+        memo = [[0]*n for _ in range(m)]
+        
+        def dfs(i,j):
+            # 到达最后一行，最后一列,终点
+            if i == m-1 and j == n-1:
+                return max(1 - dungeon[i][j],1)
+            
+            # 查询当前结果
+            if memo[i][j] > 0:
+                return memo[i][j]
+
+            # 到达最后一行,向右搜索
+            if i == m-1:
+                minRes = max(dfs(i,j+1) - dungeon[i][j],1)
+            # 到达最后一列，向下搜索
+            elif j == n-1:
+                minRes = max(dfs(i+1,j) - dungeon[i][j],1)
+            else:
+            # 向右+向下搜索
+                minRes = max(min(dfs(i,j+1) - dungeon[i][j],dfs(i+1,j) - dungeon[i][j]), 1)
+            
+            # 记录当前结果
+            memo[i][j] = minRes
+
+            return minRes
+
+        return dfs(0,0)
