f956a1309221020d958c72df5005ce7ad59f55d7
haofengsiji <xinzhengfang@qq.com>
2020-09-05 11:05:58 +0800

    update q300a1.1

--- Qustion Code/300.最长上升子序列.py
+++ Qustion Code/300.最长上升子序列.py
@@ -2,18 +2,35 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 0: return 0
+        if n == 0:
+            return 0
         dp = [0]*n
         dp[0] = 1
-        for i in range(1,n):
+        for i in range(1, n):
             max_len = 1
             for j in range(i):
                 if nums[j] < nums[i]:
-                    max_len = max(dp[j]+1,max_len)
+                    max_len = max(dp[j]+1, max_len)
             dp[i] = max_len
         return max(dp)
 
+# mehtod_1.1 6个月后
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        n = len(nums)
+        dp = [1]*n
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j]+1)
+
+        return max(dp)
+
 # method_2
+
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         d = []
